13f258be66d6ea5be76d29d0c30f5b427a054cb2
Ahmad Jawabra <ahmadjawabra95@gmail.com>
2021-09-09 03:34:06 +0300

    add loader

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
 
 let apiQuotes = [];
+
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 // // show New Quote from localQuotes
 // function newQuote() {
@@ -15,6 +28,7 @@ let apiQuotes = [];
 
 // show New Quote
 function newQuote() {
+  loading();
   // get random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // Check if Author field is blank and replace it with 'Unknown'
@@ -31,11 +45,14 @@ function newQuote() {
     quoteText.classList.remove("long-quote");
   }
 
+  // Set the quote Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 async function getQuotes() {
+  loading();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
